100d78433e4654790fd2a7a9280a321c80167788
Joe Kaiser <joe@optify.net>
2013-04-30 17:06:32 +0000

    added ps2

--- bash-profile.sh
+++ bash-profile.sh
@@ -4,5 +4,10 @@ export LSCOLORS=GxFxCxDxBxegedabagaced
 
 alias prettyjson="python -mjson.tool"
 
+function ps2
+{
+    ps ax | grep $1 --color=always
+}
+
 source ~/scripts/git-completion.sh
 source ~/scripts/git-prompt.sh
