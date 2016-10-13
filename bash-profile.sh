37d21f536cfb9f114a4ba1a6b499cabcebda6fbf
Joe <joekaiser@users.noreply.github.com>
2016-10-13 12:47:15 -0700

    Update bash-profile.sh

--- bash-profile.sh
+++ bash-profile.sh
@@ -4,30 +4,12 @@ export LSCOLORS=GxFxCxDxBxegedabagaced
 
 alias prettyjson="python -mjson.tool"
 alias reloadzsh="source ~/.zshrc"
-alias mci="mvn clean install -DskipITs -DskipTests"
-alias mcit="mvn clean install -PITEST"
-alias jira="open 'http://jira.bellevue.ad.local/secure/Dashboard.jspa'"
-alias gerrit="open 'https://gerrit.bellevue.agilysys.com:8443/#/q/status:open,n,z'"
 
 function ps2
 {
     ps ax | grep $1 --color=always
 }
 
-function aclone
-{
-  pushd ~/dev
-  git clone ssh://joek@gerrit.bellevue.agilysys.com:29418/$1.git
-  cd $1
-  scp -p -P 29418 joek@gerrit.bellevue.agilysys.com:hooks/commit-msg .git/hooks/
-  popd
-}
-
-function vag
-{
-  cd ~/dev/Vagrant-PV
-  vagrant $1
-}
 
 export JAVA_HOME=`/usr/libexec/java_home`
 export MAVEN_OPTS="-Xmx2048m -Xms512m -XX:PermSize=768m -XX:MaxPermSize=3000m"
